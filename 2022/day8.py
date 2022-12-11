ccd116b8223c3cd5fd326b59fa6eb24a8d85b0e1
Chad Maron <chadm@spotify.com>
2022-12-11 10:26:20 -0500

    Day 8 cleanup

--- 2022/day8.py
+++ 2022/day8.py
@@ -16,14 +16,19 @@ def day8a(file_name):
     n = len(grid)
     m = len(grid[0])
     visible = set()
+    max_score = 0
     dirs = [[0, -1], [0, 1], [-1, 0], [1, 0]]
     for i in range(1, n-1):
         for j in range(1, m-1):
+            scores = []
             for dx, dy in dirs:
                 new_x = i + dx
                 new_y = j + dy
+                curr_score = 0
                 hidden = False
                 while not hidden and 0 <= new_x <= n-1 and 0 <= new_y <= m-1:
+                    curr_score += 1
+
                     # We need to be taller than the neighbor AND make sure the neighbor is also visible
                     if grid[i][j] <= grid[new_x][new_y]:
                         hidden = True
@@ -34,37 +39,14 @@ def day8a(file_name):
                 if not hidden:
                     visible.add((i, j))
 
-    print(visible)
-    print(len(visible) + (2*n) + (2*m) - 4)
-
-
-def day8b(file_name):
-    grid = read_grid(file_name)
-    n = len(grid)
-    m = len(grid[0])
-    dirs = [[0, -1], [0, 1], [-1, 0], [1, 0]]
-    max_score = 0
-    for i in range(1, n-1):
-        for j in range(1, m-1):
-            scores = []
-            for dx, dy in dirs:
-                new_x = i + dx
-                new_y = j + dy
-                curr_score = 0
-                while 0 <= new_x <= n-1 and 0 <= new_y <= m-1:
-                    curr_score += 1
-                    if grid[i][j] <= grid[new_x][new_y]:
-                        break
-                    new_x += dx
-                    new_y += dy
-
                 scores.append(curr_score)
 
-            max_score = max(reduce(lambda x, y: x*y, filter(lambda x: x > 0, scores)), max_score)
+            max_score = max(reduce(lambda x, y: x * y, filter(lambda x: x > 0, scores)), max_score)
 
+    print(visible)
+    print(len(visible) + (2*n) + (2*m) - 4)
     print(max_score)
 
 
 if __name__ == '__main__':
     day8a('day8.txt')
-    day8b('day8.txt')
---
+++ 2022/day9.py
@@ -0,0 +1,13 @@
+
+
+def day9a(file_name):
+    pass
+
+
+def day9b(file_name):
+    pass
+
+
+if __name__ == '__main__':
+    day9a('day9.small.txt')
+    day9b('day9.small.txt')
